3bd20b89e9d0906816c3c0e04e1e62d4c6ad0713
traaitt <traaitt@icloud.com>
2020-06-21 01:58:23 +0200

    theme

--- website/config.js
+++ website/config.js
@@ -16,7 +16,7 @@ var blockchainExplorer = 'http://chainradar.com/{symbol}/block/{id}'
 
 var transactionExplorer = 'http://chainradar.com/{symbol}/transaction/{id}'
 
-var themeCss = 'themes/default-theme.css'
+var themeCss = 'themes/motherboard-dark-theme.css'
 
 var networkStat = {
   'bcn': [
